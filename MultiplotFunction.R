776b9733b350258dabab7d8bbb9d3ff1065dc787
Callum Macgregor <461776@hull.ac.uk>
2015-11-24 16:40:53 +0000

    Added source credit

--- MultiplotFunction.R
+++ MultiplotFunction.R
@@ -1,5 +1,7 @@
 # Multiple plot function
 #
+# Source: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#
 # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
 # - cols:   Number of columns in layout
 # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
