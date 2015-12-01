b0a3363abcc1707d7ca11deeafa7bf9c42b8997f
Callum Macgregor <461776@hull.ac.uk>
2015-12-01 12:35:52 +0000

    Function for checking glmer resids

---
+++ CheckResidsFunction.R
@@ -0,0 +1,9 @@
+chkres <- function(model) {
+  require(RVAideMemoire)
+  p1 <- plotresid(model)
+  sresid <- resid(model, type = "deviance")
+  hist(sresid)
+  fitted.glmm <- fitted(model, level=1)        # Extract the fitted (predicted) values
+  plot(sresid ~ fitted.glmm)                   # Check for homoscedasticity
+  plot(model)                                  # gives a heteroscedasticity plot #fans out, not good
+}
