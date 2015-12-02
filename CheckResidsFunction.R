25e0042234bb3f567cfea17f7439b71ed00d0fc2
Callum Macgregor <461776@hull.ac.uk>
2015-12-02 11:16:46 +0000

    Added chkres.PQL function

--- CheckResidsFunction.R
+++ CheckResidsFunction.R
@@ -1,6 +1,5 @@
 chkres <- function(model) {
   require(RVAideMemoire)
-  p1 <- plotresid(model)
   sresid <- resid(model, type = "deviance")
   hist(sresid)
   fitted.glmm <- fitted(model, level=1)        # Extract the fitted (predicted) values
@@ -8,4 +7,17 @@ chkres <- function(model) {
   plot(model)                                  # gives a heteroscedasticity plot #fans out, not good
   require(arm)
   binnedplot(fitted(model),resid(model))
+}
+
+
+
+
+chkres.PQL <- function(model) {
+  require(RVAideMemoire)
+  plot(model27a)
+  sresid <- resid(model27a, type = "pearson")
+  hist(sresid)
+  fitted.glmm <- fitted(model27a, level=1)        # Extract the fitted (predicted) values
+  plot(sresid ~ fitted.glmm)                   # Check for homoscedasticity
+  
 }
